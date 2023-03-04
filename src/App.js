66aab23e1bf320f6434548b2def854bef674deb4
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-03-04 13:43:53 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
-import './App.css'
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
 
 class App extends React.Component {
+  state = {
+    advice: '',
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
-      <h1>Random Quote Generator</h1>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
     );
   }
 }
